23ee2434a9381bc98d88573d3ea6390341776265
unknown <ononobip@gmail.com>
2023-06-28 10:36:19 +0100

    ref: refractored this code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useRouter } from 'next/router';
+import { WriteUpContext } from '../../context/WriteUpContext';
 
 type Herotype = {
     heading: string,
@@ -6,6 +8,13 @@ type Herotype = {
 }
 
 const Hero = (props: Herotype) => {
+  const {currUser, handleUserSignOut} = useContext(WriteUpContext)
+
+
+  const router = useRouter()
+  const handleRoute = () => {
+    router.push('/auth')
+  }
     const {heading, message} = props
   return (
     <div className='flex items-center justify-center h-[100vh] bg-fixed bg-center  bg-cover custom-img' id='hero'>
@@ -13,7 +22,14 @@ const Hero = (props: Herotype) => {
       <div className='p-5 text-white z-[2] max-w-[700px] mx-auto'>
         <h2 className='text-3xl sm:text-4xl font-bold'>{heading}</h2>
         <p className='py-5 text-[18px] sm:text-xl'>{message}</p>
-        <button className='px-8 py-2 border-none bg-purple-800 text-white hover:bg-white hover:text-purple-800 ease-in-out duration-300 hover:font-bold rounded-sm'>Get Started</button>
+        {
+          currUser ? (
+            <button className='px-8 py-2 border-none bg-purple-800 text-white hover:bg-white hover:text-purple-800 ease-in-out duration-300 hover:font-bold rounded-sm'>Get Unlimited Access</button>
+          ):  
+          (
+            <button onClick={handleRoute} className='px-8 py-2 border-none bg-purple-800 text-white hover:bg-white hover:text-purple-800 ease-in-out duration-300 hover:font-bold rounded-sm'>Get Started</button>
+          )
+        }
       </div>
     </div>
   )
