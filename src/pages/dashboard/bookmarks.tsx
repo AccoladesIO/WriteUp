34e0e4584dc3e03f5b20a909106b9b5868c1879a
unknown <ononobip@gmail.com>
2023-06-29 20:01:33 +0100

    update: added a coming soon page

--- src/pages/dashboard/bookmarks.tsx
+++ src/pages/dashboard/bookmarks.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Bookmarks = () => {
   return (
     <ReadersNav>
-    <div className='w-full h-screen flex items-center justify-center'>Bookmarks
+    <div className='w-full h-screen flex items-center justify-center'>
         <CommingSoon title='Bookmarks'/>
     </div>
     </ReadersNav>
