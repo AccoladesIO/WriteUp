7f723c0d5269fd92e22420a67a4cabe2de27bcb2
unknown <ononobip@gmail.com>
2023-06-29 19:42:05 +0100

    update: added a coming soon page

--- src/pages/dashboard/draft.tsx
+++ src/pages/dashboard/draft.tsx
@@ -1,12 +1,15 @@
 import CommingSoon from '@/components/CommingSoon'
+import ReadersNav from '@/components/ReadersNav'
 import React from 'react'
 
 const Draft = () => {
   return (
-    <div className='w-full h-full text-xl'>
+    <ReadersNav>
+    <div className='w-full h-full text-xl flex items-center justify-center'>
         Draft
         <CommingSoon />
     </div>
+    </ReadersNav>
   )
 }
 
