8e9dc527d5ddc110bb27d77d0c94ecee9eb64ea2
unknown <ononobip@gmail.com>
2023-06-29 19:38:44 +0100

    update: added a coming soon page

--- src/components/CommingSoon.tsx
+++ src/components/CommingSoon.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CommingSoon = () => {
   return (
-    <div className='w-full h-screen flex justify-center items-center text-3xl font-bold'>ComingSoon</div>
+    <div className='w-full h-screen flex justify-center items-center text-3xl font-bold bg-purple-200'>ComingSoon</div>
   )
 }
 
